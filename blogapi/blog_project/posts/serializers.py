37bba5acfb16b2343311dbaea59fb3742641ec62
sidi ahmed <sidiahmedhabib@gmail.com>
2024-08-11 18:48:25 +0000

    finish chap8

--- blogapi/blog_project/posts/serializers.py
+++ blogapi/blog_project/posts/serializers.py
@@ -1,8 +1,14 @@
 # posts/serializers.py
 from rest_framework import serializers
+from django.contrib.auth import get_user_model # new
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
+
+class UserSerializer(serializers.ModelSerializer): # new
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
--- blogapi/blog_project/posts/views.py
+++ blogapi/blog_project/posts/views.py
@@ -1,15 +1,18 @@
 # posts/views.py
-from rest_framework import generics, permissions # new
+from django.contrib.auth import get_user_model # new
+from rest_framework import viewsets # new
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer # new
 from .permissions import IsAuthorOrReadOnly # new
 
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+ 
+class PostViewSet(viewsets.ModelViewSet): # new
     permission_classes = (IsAuthorOrReadOnly,) # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet): # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
